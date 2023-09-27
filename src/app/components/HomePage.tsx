f4752db1bc14ad8b9384fe155d488046317e544a
Sean Michael <seanlasono@gmail.com>
2023-09-27 14:51:40 +0300

    edited homepage to add letter animation

--- src/app/components/HomePage.tsx
+++ src/app/components/HomePage.tsx
@@ -1,10 +1,7 @@
 import Link from "next/link";
-import {
-  CenterStyles,
-  center,
-  stack,
-} from "../../../styled-system/patterns";
+import { CenterStyles, center, stack } from "../../../styled-system/patterns";
 import WriteRespondButton from "./WriteRespondButton";
+import { css } from "../../../styled-system/css";
 
 export default function HomePage() {
   const buttonStyle: CenterStyles = {
@@ -12,34 +9,47 @@ export default function HomePage() {
     width: "400px",
     border: "1px solid black",
     rounded: "lg",
-    _focus:{
-      background: "gray.200"
+    _focus: {
+      background: "gray.200",
     },
-    _hover:{
+    _hover: {
       transform: "scale(1.01)",
       boxShadow: "0 5px 10px 0 rgba(0,0,0,0.19)",
-      transition: "all ease 0.1s"
-    }
+      transition: "all ease 0.1s",
+    },
   };
 
   return (
     <div
-      className={stack({
-        direction: "row",
-        justify: "center",
+      className={css({
         height: "calc(100% - 60px)",
-        gap: "15%",
       })}
     >
-      <div className={center({})}>
-        <Link href={"/writeletter"}>
-          <WriteRespondButton>Write a letter</WriteRespondButton>
-        </Link>
+      <div className={center({ p: 10, width: "100%" })}>
+        <div className={stack({ direction: "column" })}>
+          <p className={center({ fontStyle: "italic", p:4 })}>
+            Write anonymous letters and get replies from fellow strangers!
+          </p>
+        </div>
       </div>
-      <div className={center()}>
-        <Link href={"/respondletter"}>
-          <WriteRespondButton>Respond to a letter</WriteRespondButton>
-        </Link>
+      <div
+        className={stack({
+          width: "100%",
+          direction: "row",
+          justify: "center",
+          gap: "15%",
+        })}
+      >
+        <div className={center({})}>
+          <Link href={"/writeletter"}>
+            <WriteRespondButton>Write a letter</WriteRespondButton>
+          </Link>
+        </div>
+        <div className={center()}>
+          <Link href={"/respondletter"}>
+            <WriteRespondButton>Respond to a letter</WriteRespondButton>
+          </Link>
+        </div>
       </div>
     </div>
   );
--- src/app/components/WriteRespondButton.tsx
+++ src/app/components/WriteRespondButton.tsx
@@ -1,40 +1,75 @@
-"use client"
+"use client";
 
+import { css } from "../../../styled-system/css";
 import { CenterStyles, center } from "../../../styled-system/patterns";
-import {useRef, useEffect} from 'react';
+import { useRef, useEffect } from "react";
 
 export default function WriteRespondButton({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
-
   const ref = useRef(null);
 
   const buttonStyle: CenterStyles = {
+    position: "relative",
     height: "300px",
     width: "500px",
-    border: "1px solid black",
     background: "amber.300",
-    rounded: "lg",
-    _focus:{
-      background: "gray.200"
-    },
-    _hover:{
+    transition: "-webkit-transform ease-in-out 0.1s",
+    fontFamily: "monospace",
+    fontWeight: "extralight",
+    _hover: {
       transform: "scale(1.01)",
       boxShadow: "0 5px 10px 0 rgba(0,0,0,0.19)",
-      transition: "all ease 0.1s"
-    }
+    },
   };
 
-  function changeToLoading() { 
+  function changeToLoading() {
     const element: HTMLDivElement = ref.current as unknown as HTMLDivElement;
-    element.style.background = "lightgray"
-    element.style.pointerEvents = "none"
-    element.textContent = "Loading..."
+    element.style.pointerEvents = "none";
+    element.textContent = "Loading...";
   }
 
   return (
-    <div ref={ref} className={center(buttonStyle)} onClick={changeToLoading}>{children}</div>
-  )
+    <div className="group">
+      <div className={center(buttonStyle)} onClick={changeToLoading}>
+        <div
+          className={css({
+            zIndex: 5,
+            position: "absolute",
+            borderTop: "160px solid #ffbc31",
+            borderLeft: "250px solid transparent",
+            borderRight: "250px solid transparent",
+            height: 0,
+            width: 0,
+            top: 0,
+            transformOrigin: "top",
+            transition: "-webkit-transform ease-in-out 0.4s",
+            _groupHover: {
+              transform: "rotateX(160deg)",
+            },
+          })}
+        ></div>
+        <div
+          className={css({
+            position: "absolute",
+            borderTop: "160px solid #edb135",
+            borderLeft: "250px solid transparent",
+            borderRight: "250px solid transparent",
+            height: 0,
+            width: 0,
+            top: 0,
+            transformOrigin: "top",
+          })}
+        ></div>
+        <div
+          ref={ref}
+          className={css({ zIndex: "10", mt: "130px", fontSize: "2rem" })}
+        >
+          {children}
+        </div>
+      </div>
+    </div>
+  );
 }
