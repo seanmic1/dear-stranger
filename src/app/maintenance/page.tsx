5e810f0da4762e4bc119f6663de63bb9b4406483
Sean Michael <seanlasono@gmail.com>
2023-10-14 10:17:43 +0300

    adjusted maintenance formatting

--- src/app/maintenance/page.tsx
+++ src/app/maintenance/page.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import { center } from '../../../styled-system/patterns'
 
 export default function Maintenance() {
   return (
-    <div>Sorry, website is currently undermaintenance! Please come again later.</div>
+    <div className={center({height:"100vh", textAlign:"center"})}>Sorry, website is currently undermaintenance. <br/> Please come again later.</div>
   )
 }
