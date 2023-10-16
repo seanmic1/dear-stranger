37dd3e50c22814f881263679954882c5cc340393
Sean Michael <seanlasono@gmail.com>
2023-10-16 17:18:17 +0300

    fixed maintenance page spelling lol

--- src/app/maintenance/page.tsx
+++ src/app/maintenance/page.tsx
@@ -3,6 +3,6 @@ import { center } from '../../../styled-system/patterns'
 
 export default function Maintenance() {
   return (
-    <div className={center({height:"100vh", textAlign:"center"})}>Sorry, website is currently undermaintenance. <br/> Please come again later.</div>
+    <div className={center({height:"100vh", textAlign:"center"})}>Sorry, website is currently under maintenance. <br/> Please come again later.</div>
   )
 }
